a4cfc716efe0c3ea71ae56879eea52ed384891fc
Paolo Flores <maximo_150@hotmail.com>
2025-03-25 21:37:31 -0500

    commit 2025-03-25 21:37

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,28 @@
-import "./globals.css";
+import "./globals.css"
+import LeftBar from "@/components/LeftBar"
+import RightBar from "@/components/RightBar"
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* <div className="flex justify-between bg-red-300 xsm:bg-yellow-200 lg:bg-green-500 xxl:bg-pink-400"> */}
+        <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
+          <div className="px-2 xsm:px-4 xxl:px-8">
+            <LeftBar /> {/* Componente */}
+          </div>
+
+          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray">{children}</div>
+
+          <div className="hidden lg:flex ml-4 md:ml-8 flex-1">
+            <RightBar /> {/* Componente */}
+          </div>
+        </div>
+      </body>
     </html>
-  );
+  )
 }
---
+++ src/components/LeftBar.tsx
@@ -0,0 +1,5 @@
+export default function LeftBar() {
+  return (
+    <div>LeftBar</div>
+  )
+}
